b5cefec0f33ef39a65c43c823a5dac69ebfb3f90
Divan Visagie <visagiedivan@gmail.com>
2016-04-11 21:28:38 +0200

    calculator functionality

--- app/src/main/java/com/dvisagie/calculator/StringCalculator.kt
+++ app/src/main/java/com/dvisagie/calculator/StringCalculator.kt
@@ -3,7 +3,33 @@ package com.dvisagie.calculator
 
 class StringCalculator {
 
-    fun calculate(s: String): Int {
-        return 4
+
+
+    fun calculate(calculationList: List<String>): Int {
+        var currentOp: String = ""
+        var currentNumber = 0
+
+        calculationList.forEach { token ->
+            when {
+                token.equals("+")
+                || token.equals("/")
+                || token.equals("*")
+                || token.equals("-") -> currentOp = token
+                else -> {
+                    when {
+                        currentOp.equals("-") -> currentNumber -= token.toInt()
+                        currentOp.equals("/") -> currentNumber /= token.toInt()
+                        currentOp.equals("*") -> currentNumber *= token.toInt()
+                        else -> {
+                            currentNumber += token.toInt()
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return currentNumber
+
     }
 }
--- app/src/test/java/com/dvisagie/calculator/StringCalculatorUnitTest.kt
+++ app/src/test/java/com/dvisagie/calculator/StringCalculatorUnitTest.kt
@@ -13,9 +13,48 @@ class StringCalculatorUnitTest {
     }
 
     @Test
-    fun given2plus2_ShouldReturn_4() {
+    fun given_2_plus2_Should_Return_4() {
+
+        val operationList: List<String> = arrayListOf("2","+","2")
+
         val calculator = StringCalculator()
-        val answer = calculator.calculate("1+1")
+        val answer = calculator.calculate(operationList)
         assertEquals(4, answer.toLong())
+    }
+
+    @Test
+    fun given_4_plus_2_Should_Return_6() {
+        val operationList: List<String> = arrayListOf("4","+","2")
+
+        val calculator = StringCalculator()
+        val answer = calculator.calculate(operationList)
+        assertEquals(6, answer.toLong())
+    }
+
+    @Test
+    fun given_4_minus_2_Should_Return_2() {
+        val operationList: List<String> = arrayListOf("4","-","2")
+
+        val calculator = StringCalculator()
+        val answer = calculator.calculate(operationList)
+        assertEquals(2, answer.toLong())
+    }
+
+    @Test
+    fun given_6_divided_by_2_should_return_3() {
+        val operationList: List<String> = arrayListOf("6","/","2")
+
+        val calculator = StringCalculator()
+        val answer = calculator.calculate(operationList)
+        assertEquals(3, answer.toLong())
+    }
+
+    @Test
+    fun given_2_multiplied_by_3_should_return_6() {
+        val operationList: List<String> = arrayListOf("2","*","3")
+
+        val calculator = StringCalculator()
+        val answer = calculator.calculate(operationList)
+        assertEquals(6, answer.toLong())
     }
 }
